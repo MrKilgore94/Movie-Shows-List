afc9fd22cf5e5fe85f915eb62d0479c1180b0dd4
Austin Kilgore <mr.kilgore@Austins-MacBook-Air.local>
2022-02-22 23:19:23 -0700

    added book

--- ReadingList.js
+++ ReadingList.js
@@ -3,13 +3,15 @@
 let origBookList = [
   {id: 1 , title: 'Tom Clancy: Rainbow six', read: true}, 
   {id: 2 , title: 'Tom Clancy: Enemy Contact', read: true},
-  {id: 3 , title: 'Tom Clancy: Duty and Honor' , read: false}
+  {id: 3 , title: 'Tom Clancy: Duty and Honor' , read: false},
+  {id: 4 , title: 'Tom Clancy: Without Remorse' , read: false}
+
   
   ]
   console.log(origBookList)
   
   
-  const newBookToAdd = {id: 3, title: "Tom Clancy: Power and Empire", read: false}
+  const newBookToAdd = {id: 5, title: "Tom Clancy: Power and Empire", read: false}
   
   
   const addNewBook = (newBook)=> {
